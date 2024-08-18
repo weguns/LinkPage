ac334f72e9e94411af69e351a17a007e97d34d9a
weguns <105779650+weguns@users.noreply.github.com>
2024-08-18 17:33:36 +0330

    Add files via upload

---
+++ scripts/gameScript.js
@@ -0,0 +1,36 @@
+const object = document.getElementById('object');
+
+document.body.addEventListener('keydown', (e) => handelGameMoves(e))
+document.body.addEventListener('keyup', (e) => {
+    if (e.keyCode === 68 || e.keyCode === 65) {
+        object.classList.add('stop');
+        object.classList.remove('move');
+    }
+})
+
+let objectXPos = 0;
+let objectAlign = 1;
+
+function handelGameMoves(e) {
+    if (e.keyCode === 65) {
+        objectXPos -= 7.5;
+        objectAlign = -1;
+    } else if (e.keyCode === 68) {
+        objectXPos += 7.5;
+        objectAlign = 1;
+    } else if (e.keyCode === 32) {
+        object.classList.add('jump');
+        setTimeout(() => {
+            object.classList.remove('jump')
+            object.classList.add('stop');
+            object.classList.remove('move');
+        }, 1000)
+    }
+
+    if (e.keyCode === 65 || e.keyCode === 68 || e.keyCode === 32) {
+        object.classList.remove('stop');
+        object.classList.add('move')
+    }
+    object.style.transform = `translateX(${objectXPos}px) scaleX(${objectAlign})`;
+    console.log(e.keyCode)
+}
