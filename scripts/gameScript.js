d2b76ec1b96e61752d1dff8d4d29620a027ba58f
weguns <105779650+weguns@users.noreply.github.com>
2024-08-18 20:48:19 +0330

    Add keyboard for game

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -1,12 +1,26 @@
-alert('Page is currently only available on desktop.\nGame Controls => D:Right A:Left Space:Jump')
+// alert('Page is currently only available on desktop.\nGame Controls => D:Right A:Left Space:Jump')
 
 const object = document.getElementById('object');
+const jumpBut = document.querySelector('#jump');
+const rightBut = document.querySelector('#right');
+const leftBut = document.querySelector('#left');
 
 document.body.addEventListener('keydown', (e) => handelGameMoves(e))
 document.body.addEventListener('keyup', (e) => {
     if (e.keyCode === 68 || e.keyCode === 65) {
         object.classList.add('stop');
         object.classList.remove('move');
+    }
+
+    if (e.keyCode === 65) {
+        leftBut.classList.remove('activeBut');
+        leftBut.classList.add('deactivateBut');
+    } else if (e.keyCode === 68) {
+        rightBut.classList.remove('activeBut');
+        rightBut.classList.add('deactivateBut');
+    } else if (e.keyCode === 32) {
+        jumpBut.classList.remove('activeBut');
+        jumpBut.classList.add('deactivateBut');
     }
 })
 
@@ -32,6 +46,17 @@ function handelGameMoves(e) {
     if (e.keyCode === 65 || e.keyCode === 68 || e.keyCode === 32) {
         object.classList.remove('stop');
         object.classList.add('move')
+    }
+
+    if (e.keyCode === 65) {
+        leftBut.classList.add('activeBut');
+        leftBut.classList.remove('deactivateBut');
+    } else if (e.keyCode === 68) {
+        rightBut.classList.add('activeBut');
+        rightBut.classList.remove('deactivateBut');
+    } else if (e.keyCode === 32) {
+        jumpBut.classList.add('activeBut');
+        jumpBut.classList.remove('deactivateBut');
     }
     object.style.transform = `translateX(${objectXPos}px) scaleX(${objectAlign})`;
     console.log(e.keyCode)
